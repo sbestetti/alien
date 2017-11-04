73d514b1a6e36e502a187910a8ff3417c53ce3af
Sergio B <sbestetti@gmail.com>
2017-11-04 15:18:30 +0000

    game_functions refactoring

--- game_functions.py
+++ game_functions.py
@@ -2,25 +2,30 @@ import sys
 import pygame
 
 
+def check_key_down(ship, event):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_key_up(ship, event):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Respond to user input"""
     for event in pygame.event.get():
-
         """Event monitor"""
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_key_down(ship, event)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_key_up(ship, event)
 
 
 def update_screen(screen, ship, ai_settings):
--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         self.screen_width = 1200
         self.scree_height = 800
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 1.50
+        self.ship_speed_factor = 4.00
