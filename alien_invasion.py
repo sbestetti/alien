f4374601d27aecbb7ff064568100447bab8de4b9
Sergio B <sbestetti@gmail.com>
2017-11-04 11:24:22 +0000

    Created a Settings class and module

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,18 +3,26 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     """Initialize pygame, create game window and start main loop"""
+    ai_settings = Settings()
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.scree_height))
     pygame.display.set_caption("Alien Invasion")
 
     while True:
         """Main game loop"""
+
         for event in pygame.event.get():
+            """Event monitor"""
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(ai_settings.bg_color)
+
         pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Class to store all game settings"""
+
+    def __init__(self):
+        """Screen settings"""
+        self.screen_width = 1200
+        self.scree_height = 800
+        self.bg_color = (230, 230, 230)
