1df2eb2e0d547e2ddc031ec428b78619d3a33e61
Sergio B <sbestetti@gmail.com>
2017-11-04 16:29:16 +0000

    New class to manage bullets

--- settings.py
+++ settings.py
@@ -6,4 +6,12 @@ class Settings:
         self.screen_width = 1200
         self.scree_height = 800
         self.bg_color = (230, 230, 230)
+
+        """Ship settings"""
         self.ship_speed_factor = 4.00
+
+        """Bullet settings"""
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
